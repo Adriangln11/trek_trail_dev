63f4dfc26012d8c07dc6c3b3da22e889e7d20baa
MartinPC <MartinChrisPC@hotmail.com>
2024-04-01 22:07:03 -0500

    ADD Códigos de estado HTTP

--- backend/src/utils/constants.ts
+++ backend/src/utils/constants.ts
@@ -1,7 +1,15 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
 export const port: string | undefined = process.env.PORT
 export const db: string = process.env.DB || ''
 export const secretKey: string = process.env.JWT_SECRET || ''
+
+export enum CODE {
+	BAD_REQUEST = 400,
+	NOT_FOUND = 404,
+	CREATED = 201,
+	OK = 200,
+	INTERNAL_SERVER_ERROR = 500
+}
