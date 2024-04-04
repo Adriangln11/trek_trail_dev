4194cd3a01c60f4a2db4c0fce8697606b7ce93cd
MartinPC <MartinChrisPC@hotmail.com>
2024-04-04 10:39:55 -0500

    UPDATE Place Model

--- backend/src/models/place.model.ts
+++ backend/src/models/place.model.ts
@@ -1,19 +1,25 @@
-import mongoose, { Schema, Document } from 'mongoose';
-import { placesInterface } from '../interfaces/places.interface';
+import mongoose, { Schema, Document, Types } from 'mongoose'
+import { placesInterface } from '../interfaces/places.interface'
+import CommentModel from './comment.model'
 
+const commentSchema = new Schema(
+	{
+		cid: { type: Types.ObjectId, ref: CommentModel }
+	},
+	{ _id: false }
+)
 
 const PlaceSchema = new Schema<placesInterface & Document>({
-  id: { type: String },
-  name: { type: String, required: true },
-  location: { type: String, required: true },
-  country: { type: String, required: true },
-  commentsId: [{
-    "_id": false,
-    cid: { type: mongoose.Schema.Types.ObjectId, ref: 'Comment' },
-  }]
-});
+	id: { type: String },
+	name: { type: String, required: true },
+	location: { type: String, required: true },
+	country: { type: String, required: true },
+	commentsId: [commentSchema]
+})
 
+const PlaceModel = mongoose.model<placesInterface & Document>(
+	'Place',
+	PlaceSchema
+)
 
-const PlaceModel = mongoose.model<placesInterface & Document>('Place', PlaceSchema);
-
-export default PlaceModel;
+export default PlaceModel
