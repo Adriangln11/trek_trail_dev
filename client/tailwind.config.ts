cb93d5bca457fb658d66acad11a2132b44603eaa
Lucas Clemente <lucasclemente2016@gmail.com>
2024-04-05 18:43:48 -0300

    Fix Tailwind config

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -8,11 +8,24 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'soft-gray': '#807A80',
+        'dark-green': '#073420',
+        'teal': '#497574',
+        'forest-green': '#1D3B31',
+        'silver': '#C0C0C0',
+        'light-blue': '#081726',
+        'dark-green-yellow': '#2F401E',
+        'olive': '#4F5904',
+        'dark-silver': '#VBFBDB4', 
+        'mystery-color': '#t88gc07',
       },
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+        aeonik: ['Aeonik', 'sans-serif']
+      },
+      
     },
   },
   plugins: [],
